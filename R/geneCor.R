fc038182b7f06ca9dc30d483e7e57842436aae70
Quang-Huy Nguyen <30990121+huynguyen250896@users.noreply.github.com>
2020-09-29 15:52:10 +0700

    Update geneCor.R
    The function can compute correlation coefficients following the Pearson, Spearman's rank, or Kendall method.

--- R/geneCor.R
+++ R/geneCor.R
@@ -11,7 +11,7 @@
 #' @export
 
 # identify significantly correlated genes
-geneCor = function(cna = NULL, exp1 = NULL, alternative1=c("two.sided","less","greater"), met = NULL, exp2 = NULL, alternative2=c("two.sided","less","greater"))
+geneCor = function(cna = NULL, exp1 = NULL, alternative1=c("two.sided","less","greater"), met = NULL, exp2 = NULL, alternative2=c("two.sided","less","greater"), method = "pearson")
 {
   #library
   library(tidyr)
@@ -49,7 +49,7 @@ geneCor = function(cna = NULL, exp1 = NULL, alternative1=c("two.sided","less","g
   estimates = numeric(ncol(cna))
   pvalues = numeric(ncol(cna))
   for (i in c(1:ncol(cna))) {
-    cc=cor.test(cna[,i],exp1[,i])
+    cc=cor.test(cna[,i],exp1[,i], method = method)
     cna_cor$Estimate[i]=cc$estimate
     cna_cor$P.value[i]=cc$p.value
     rownames(cna_cor) = colnames(cna)[1:ncol(cna)]
@@ -64,7 +64,7 @@ geneCor = function(cna = NULL, exp1 = NULL, alternative1=c("two.sided","less","g
   estimates = numeric(ncol(met))
   pvalues = numeric(ncol(met))
   for (i in c(1:ncol(met))) {
-    cc2=cor.test(met[,i],exp2[,i])
+    cc2=cor.test(met[,i],exp2[,i], method = method)
     met_cor$Estimate[i]=cc2$estimate
     met_cor$P.value[i]=cc2$p.value
     rownames(met_cor) = colnames(met)[1:ncol(met)]
