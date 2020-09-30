e2b90f373d59c4505015a725fe6ebd27e4781eff
Quang-Huy Nguyen <30990121+huynguyen250896@users.noreply.github.com>
2020-09-30 15:38:47 +0700

    Update geneCor.R

--- R/geneCor.R
+++ R/geneCor.R
@@ -2,11 +2,11 @@
 #'
 #' @description  It automatically individually computes the correlation coefficients of genes shared between CNA data and the corresponding mRNA, and those shared between MET data and the corresponding mRNA; visualizes the overall distribution of Z values between MET or CNA and the corresponding mRNA on a page; and examines the significance of the skewness for those distributions using D'Agostino test.
 #'
-#' @param cna,exp1,alternative1,met,exp2,alternative2
+#' @param cna,exp1,alternative1,met,exp2,alternative2, method
 #'
 #' @return NULL
 #'
-#' @examples geneCor(cna = df1, exp1 = df2, alternative1="less", met = df3, exp2 = df4, alternative2="greater")
+#' @examples geneCor(cna = df1, exp1 = df2, alternative1="less", met = df3, exp2 = df4, alternative2="greater", method = "pearson")
 #'
 #' @export
 
